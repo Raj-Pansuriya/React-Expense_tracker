b98c59200ecb5fd588f748e0518b7e68df20461d
Raj <rajpansuriya40@gmail.com>
2023-02-21 17:30:08 +0530

    feat: Handle the form inputs using multiple inputs

--- src/components/NewExpense/ExpenseForm.js
+++ src/components/NewExpense/ExpenseForm.js
@@ -1,22 +1,42 @@
-import "./ExpenseForm.css"
+import { useState } from "react";
+
+import "./ExpenseForm.css";
 
 export default function ExpenseForm() {
+
+    const [enteredTitle, setEnteredTitle] = useState("");
+    const [enteredAmount, setEnteredAmount] = useState("");
+    const [enteredDate, setEnteredDate] = useState("");
+
+
+    const titleChangeHandler = (event) => {
+        setEnteredTitle(event.target.value);
+    }
+
+    const amountChangeHandler = (event) => {
+        setEnteredAmount(event.target.value)
+    }
+
+    const dateChangeHandler = (event) => {
+        setEnteredDate(event.date.value);
+    }
+
     return (
         <form>
             <div className="new-expense__controls">
                 <div className="new-expense__control">
                     <title>Title</title>
-                    <input type="text" />
+                    <input type="text" onChange={titleChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <title>Amount</title>
-                    <input type="number" min="0.01" step="0.01" />
+                    <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
                 </div>
 
                 <div className="new-expense__control">
                     <title>Title</title>
-                    <input type="date" min="2019-01-01" max="2023-01-31" />
+                    <input type="date" min="2019-01-01" max="2023-01-31" onChange={dateChangeHandler} />
                 </div>
             </div>
 
