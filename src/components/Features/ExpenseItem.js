b8d24eeff68606663e2944dc73050e05ec0716c8
Raj Pansuriya <raj.pansuriya@mindbodyonline.com>
2023-02-24 13:36:10 +0530

    refactor: Remove the demo button
    Removed the demo button created to understand states and events in React

--- src/components/Features/ExpenseItem.js
+++ src/components/Features/ExpenseItem.js
@@ -1,29 +1,16 @@
-import { useState } from "react";
-
 import "./ExpenseItem.css";
 import ExpenseDate from "./ExpenseDate";
 import Card from "../UI/Card";
 
 function ExpenseItem(props) {
 
-    const [title, setTitle] = useState(props.title)
-
-    // A React hook must DIRECTLY called in the component function.
-    // It should never be called outside of the function or even not
-    // in a nested function like clickHandler.
-    const clickHandler = () => {
-        setTitle("Updated!")
-        console.log(title);
-    };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date}></ExpenseDate>
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">${props.amount}</div>
             </div>
-            <button onClick={clickHandler}>Change Title</button>
         </Card>
     );
 }
